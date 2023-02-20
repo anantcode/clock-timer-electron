127cc533d74b0320b823cfb74fdc76f8c11ffa9f
Anant <anant.code@gmail.com>
2023-02-20 15:00:50 +0530

    Draw an animated increasing arc from CENTER of circle

--- renderer.js
+++ renderer.js
@@ -5,23 +5,24 @@ const ctx = canvas.getContext('2d');
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 const radius = 50;
-const startAngle = 0;
-const endAngle = 2 * Math.PI;
+const startAngle = -Math.PI / 2; // Starting angle shifted to top
+let endAngle = 0;
 
 // Set the timer parameters
 let totalTime = 10; // Total time in seconds
 let remainingTime = totalTime; // Remaining time in seconds
-let timeInterval = 100; // Time interval in milliseconds
+let timeInterval = 10; // Time interval in milliseconds
 let timePerFrame = 1 / (totalTime * 1000 / timeInterval); // Fraction of circle to draw per frame
 let currentAngle = startAngle;
 
 // Set the circle color
-ctx.fillStyle = 'red';
+ctx.strokeStyle = 'red';
+ctx.lineWidth = 10;
 
 // Draw the initial circle
 ctx.beginPath();
 ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-ctx.fill();
+ctx.stroke();
 
 // Update the circle every timeInterval milliseconds
 let intervalId = setInterval(() => {
@@ -30,11 +31,12 @@ let intervalId = setInterval(() => {
   
   // Calculate the current angle based on the remaining time
   currentAngle = (totalTime - remainingTime) / totalTime * 2 * Math.PI;
+  endAngle = currentAngle + startAngle; // End angle calculated from current angle
   
-  // Draw the circle
+  // Draw the colored arc
   ctx.beginPath();
-  ctx.arc(centerX, centerY, radius, startAngle, currentAngle);
-  ctx.fill();
+  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+  ctx.stroke();
   
   // Update the remaining time
   remainingTime -= timeInterval / 1000;
